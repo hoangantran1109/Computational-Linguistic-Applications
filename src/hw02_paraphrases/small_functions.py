c59ee213a983fc31c3d1d5badc6e06f2ac53624b
Renzdy <hoangantran1109@gmail.com>
2020-05-13 20:04:53 +0200

    Homework 2 submission
    Aufgabe 2.1 und 2.2 !

--- src/hw02_paraphrases/small_functions.py
+++ src/hw02_paraphrases/small_functions.py
@@ -9,9 +9,11 @@ def square_roots(start, end, length):
     >>> square_roots(4,9,3)
     array([2.        , 2.54950976, 3.        ])
     """
-    pass  # TODO: Exercise 2.1
-
-
+    # TODO: Exercise 2.1
+    equidistant=(start+end)/2
+    hilf_list=[start,equidistant,end]
+    array=np.sqrt(hilf_list)
+    return array[:length]
 def odd_ones_squared(rows, cols):
     """
     Returns a 2d numpy array with shape (rows, cols). The matrix cells contain increasing numbers,
@@ -22,4 +24,11 @@ def odd_ones_squared(rows, cols):
            [ 25,   6,  49,   8,  81],
            [ 10, 121,  12, 169,  14]])
     """
-    pass  # TODO: Exercise 2.2
+    # TODO: Exercise 2.2
+    array=np.arange(rows*cols).reshape(rows,cols)
+    odd_list=[x for x in range(rows*cols) if x%2==1]
+    for i in range(rows):
+        for j in range(cols):
+            if(array[i][j] in odd_list):
+             array[i][j]*=array[i][j]
+    return array
